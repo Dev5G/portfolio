9b40e15e7177a7083611cf41c7257fe0b5377858
Shahid Hussain <shahid.sgt@outlook.com>
2021-12-14 02:20:48 +0500

    started working on portfolio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyles from "./styles.g"
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  return <>
+    <GlobalStyles />
+    <ThemeProvider theme={lightTheme}>
+      App File
+
+    </ThemeProvider>
+  </>
+
 }
 
 export default App
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+	return (
+		<div>
+			Main Componenet
+		</div>
+	)
+}
+
+export default Main
